25e7ff206619bd85124e57fa1406bdd95b3b5a19
manish.khadka <manishkhadka362@gmail.com>
2025-02-05 11:12:50 +0545

    Update the config and yml file

--- src/test/Karate-config.js
+++ src/test/Karate-config.js
@@ -1,14 +1,15 @@
 function fn() { 
-  
-  var config = {
-  JSONPLACEHOLDER : 'https://jsonplaceholder.typicode.com/',
-  FAKERESTAPI : 'https://fakerestapi.azurewebsites.net/',
-  Author : 'QA Manish Khadka'
-	   
-  } 
+  var config = {};
+
+  // Read from system properties or environment variables
+  config.FAKERESTAPI = karate.properties['FAKERESTAPI'] || java.lang.System.getenv('FAKERESTAPI') || 'https://fakerestapi.azurewebsites.net/';
+
+  if (!config.FAKERESTAPI) {
+    throw new Error("FAKERESTAPI is not set. Check environment variables or system properties.");
+  }
 
   karate.configure('connectTimeout', 50000);
   karate.configure('readTimeout', 50000);
 
   return config;
- }
+}
