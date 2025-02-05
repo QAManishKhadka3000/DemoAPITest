cbed55fafb28442d6f53201a4c2c53a6d6f89796
manish.khadka <manishkhadka362@gmail.com>
2025-02-05 11:48:16 +0545

    Updated the YML file

--- src/test/resources/Karate-config.js
+++ src/test/resources/Karate-config.js
@@ -1,12 +1,12 @@
-function fn() { 
-  var config = {}; 
+function fn() {
+	var config = {};
 
-  // Read from system properties or environment variables
-  config.FAKERESTAPI = 'https://fakerestapi.azurewebsites.net/';
-  config.Author = 'QA Manish Khadka';
+	// Accessing environment variable properly using karate.properties
+	config.FAKERESTAPI = karate.properties['FAKERESTAPI'] || 'https://fakerestapi.azurewebsites.net/';
+	config.Author = 'QA Manish Khadka';
 
-  karate.configure('connectTimeout', 50000);
-  karate.configure('readTimeout', 50000);
+	karate.configure('connectTimeout', 50000);
+	karate.configure('readTimeout', 50000);
 
-  return config;
+	return config;
 }
