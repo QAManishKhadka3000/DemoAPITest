a6a984da8696a4f19c26d3282115a096934a3987
Manish khadka <58542906+QAManishKhadka3000@users.noreply.github.com>
2025-02-05 11:01:09 +0545

    Update Karate-config.js

--- src/test/Karate-config.js
+++ src/test/Karate-config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Karate-config.js
 function fn() { 
   
   var config = {
@@ -15,18 +14,3 @@ function fn() {
 
   return config;
  }
-=======
-function fn() { 
-  
-  var config = {
-   JSONPLACEHOLDER : 'https://jsonplaceholder.typicode.com/',
-  FAKERESTAPI : 'https://fakerestapi.azurewebsites.net/',
-   Author : 'QA Manish Khadka'
-	   
-  } 
-
-  karate.configure('connectTimeout', 50000);
-  karate.configure('readTimeout', 50000);
-  return config;
- }
->>>>>>> 1180d718b7a691593e46a7f0b336d984eca692ae:src/test/Karate-config.js
